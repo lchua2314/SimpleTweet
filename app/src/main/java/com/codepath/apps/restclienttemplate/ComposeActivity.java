2a7248eef48ca1eaac0fec681167e165bbc59d81
lchua2314 <lchua2314@gmail.com>
2021-02-18 00:29:22 -0800

    Added user story: character count

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,11 +3,16 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -21,10 +26,12 @@ import okhttp3.Headers;
 public class ComposeActivity extends AppCompatActivity {
 
     public static final String TAG = "ComposeActivity";
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 280;
 
     EditText etCompose;
     Button btnTweet;
+    TextView tvCharCount;
+    ColorStateList oldColors;
 
     TwitterClient client;
 
@@ -37,6 +44,8 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCharCount = findViewById(R.id.tvCharCount);
+        oldColors =  tvCharCount.getTextColors(); //save original colors
 
         // Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +84,36 @@ public class ComposeActivity extends AppCompatActivity {
                         Log.e(TAG, "onFailure to publish tweet", throwable);
                     }
                 });
+            }
+        });
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Fires right as the text is being changed (even supplies the range of text)
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+
+                // If the total characters exceeds 280,
+                // 1. set character counter text to red
+                // 2. disable Tweet button
+                // Else, undo the above
+                if (s.toString().length() > MAX_TWEET_LENGTH) {
+                    tvCharCount.setTextColor(Color.RED);
+                    btnTweet.setEnabled(false);
+                } else {
+                    tvCharCount.setTextColor(oldColors);
+                    btnTweet.setEnabled(true);
+                }
+                tvCharCount.setText(s.toString().length() + "/280");
             }
         });
     }
